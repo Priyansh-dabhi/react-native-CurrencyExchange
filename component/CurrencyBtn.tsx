c2f6a943e1734d06913cf848b565b2f6dcd6c34b
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-06-12 20:53:11 +0530

    completed!

--- component/CurrencyBtn.tsx
+++ component/CurrencyBtn.tsx
@@ -9,23 +9,31 @@ type CurrencyButtonProps = PropsWithChildren<{
 
 const CurrencyBtn = (props:CurrencyButtonProps): React.JSX.Element =>{
   return (
-    <View>
-      <Text>CurrencyBtn</Text>
-    </View>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.flag}>{props.flag}</Text>
+        <Text style={styles.country}>{props.name}
+        </Text>
+      </View>
   )
 }
 
 const styles = StyleSheet.create({
-     buttonContainer : {
-        alignItems: 'center'
+  buttonContainer : {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingBottom:10,
+      height:60,
+      width:100,
+      // borderWidth:1
+      // backgroundColor:'red',
     },
     flag: {
-        fontSize: 28,
+        fontSize: 22,
         color: "#FFFFFF",
         marginBottom: 4
     },
     country: {
-        fontSize: 14,
+        fontSize: 12,
         color: "#2d3436",
     
     }
